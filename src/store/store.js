d6018c598155a60244595d30e2a456fda15cc270
Damien Flury <damien.flury@gmail.com>
2019-08-19 14:50:30 +0200

    Add weather to redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import {
+  createStore, combineReducers, applyMiddleware, compose,
+} from 'redux';
+import thunk from 'redux-thunk';
 import settings from './reducers/settings-reducer';
+import weather from './reducers/weather-reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   settings,
+  weather,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
