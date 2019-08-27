3b8fb0fceac9aa95406a2835d289675c3ae8c060
Damien Flury <damien.flury@gmail.com>
2019-08-27 16:54:12 +0200

    Sanitize input

--- src/components/stats/time-helpers.js
+++ src/components/stats/time-helpers.js
@@ -1,13 +1,18 @@
 
 
-export const calculatePercentage = (fullTimeSpan, partialTimeSpan) => (partialTimeSpan / fullTimeSpan * 100 >= 100 ? 100 : partialTimeSpan / fullTimeSpan * 100);
+export const calculatePercentage = (fullTimeSpan, partialTimeSpan) => {
+  const percentageValue = partialTimeSpan / fullTimeSpan * 100;
+  if (percentageValue > 100) return 100;
+  if (percentageValue < 0) return 0;
+  return percentageValue;
+};
 
 export const humanizeWithMinutes = (duration) => {
   if (duration.hours() === 0) {
-    return `${duration.minutes()} minutes`;
+    return `${Math.abs(duration.minutes())} minutes`;
   }
   if (duration.minutes === 0) {
-    return `${duration.hours()} hours`;
+    return `${Math.abs(duration.hours())} hours`;
   }
   return `${Math.abs(duration.hours())} hours and ${Math.abs(duration.minutes())} minutes`;
 };
