300689993a6b281ab03c7b4f25a81f7c50682a32
Damien Flury <damien.flury@gmail.com>
2019-09-16 14:38:37 +0200

    Readd stuff

---
+++ src/components/Widgets/Widgets.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import Workday from './Workday/Workday';
+import Weather from './Weather';
+import Quote from './Quote';
+import Time from './Time';
+
+const Grid = styled.div`
+  display: grid;
+  margin: 20px;
+  grid-gap: 20px;
+  grid-template-columns: 2fr 1fr;
+  grid-template-areas:
+  "time time"
+  "workday workday"
+  "weather weather"
+  "quote quote";
+  @media(min-width: 1024px) {
+    grid-template-areas: "workday time"
+    "workday weather"
+    "workday quote";
+  }
+`;
+
+const StyledWorkday = styled(Workday)`
+  grid-area: workday;
+`;
+const StyledTime = styled(Time)`
+  grid-area: time;
+`;
+const StyledWeather = styled(Weather)`
+  grid-area: weather;
+`;
+const StyledQuote = styled(Quote)`
+  grid-area: quote;
+`;
+
+const Widgets = () => (
+  <Grid>
+    <StyledWorkday />
+    <StyledTime />
+    <StyledWeather />
+    <StyledQuote />
+  </Grid>
+);
+
+export default Widgets;
